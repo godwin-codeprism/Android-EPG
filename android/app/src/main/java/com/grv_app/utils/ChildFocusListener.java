bddec664a3d6df59f656bad22130d324ba8ebe95
godwin-codeprism <godwin.vc@codeprism.in>
2019-09-25 17:35:59 +0530

    Added Horizontal scroll with sync.

---
+++ android/app/src/main/java/com/grv_app/utils/ChildFocusListener.java
@@ -0,0 +1,8 @@
+package com.grv_app.utils;
+
+/**
+ * Created by Godwin Vinny Carole K on Wed, 25 Sep 2019 at 11:40.
+ * Copyright (c) Code Prism Technologies Pvt Ltd
+ */
+public interface ChildFocusListener {
+}
